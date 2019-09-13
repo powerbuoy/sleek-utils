3dd5af2fb0057914a5c7db7842bac18a5437bcac
AL <powerbuoy@gmail.com>
2019-09-13 16:46:09 +0200

    Moar

--- sleek-utils.php
+++ sleek-utils.php
@@ -7,7 +7,6 @@ namespace Sleek\Utils;
 # http://ryanfrankel.com/how-to-find-the-number-of-words-in-a-post-in-wordpress/
 function get_reading_time ($postId, $wordsPerMinute = 200) {
 	$numWords = str_word_count(strip_tags(get_post_field('post_content', $postId)));
-	$min = ceil($numWords / $wordsPerMinute);
 
-	return $min;
+	return ceil($numWords / $wordsPerMinute);
 }
